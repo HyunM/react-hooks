3a4184bccc6b7c2de173c7d87a86c819ba43f975
HyunM <wisek0226@gmail.com>
2020-07-23 08:08:26 -0700

    intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Axios from "axios";
 
-function App() {
-  //Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+function useInput(defaultValue) {
+  const [value, setValue] = useState(defaultValue);
+
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e;
+    setValue(value);
+  };
+
+  return { value, onChange };
+}
+
+function useFetch(url) {
+  const [payload, setPayload] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const callUrl = async () => {
+    try {
+      const { data } = await Axios.get(url);
+      setPayload(data);
+    } catch {
+      setError("hello from setError");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    callUrl();
+  }, []);
+  //this means ComponentDidMount. not ComponentDidUpdate
+
+  return { payload, loading, error };
+}
+
+const dataUrl = {
+  file: "https://purr.objects-us-east-1.dream.io/i/sexy.jpeg",
+};
+
+export default function App() {
+  const name = useInput("");
+  const { payload, loading, error } = useFetch(dataUrl);
+  console.log(name);
   return (
-    <div>
-      <p>You clicked {count}times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+    <div className="App">
+      <h1>Use Hooks</h1>
+      <br />
+      <input {...name} placeholder="Whats your name" />
+      {/* <input value={name.value} onChange={name.onChange} placeholder="Whats your name" /> */}
+      <br />
+      {loading && <span>loading your cat</span>}
+      {!loading && error && <span>{error}</span>}
+      {!loading && payload && <img src={payload.file} />}
     </div>
   );
 }
-
-export default App;
