8810f154cfc00353ee8effe43b21b33d05194ecd
HyunM <wisek0226@gmail.com>
2020-07-23 15:34:46 -0700

    useFadeIn

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useBeforeLeave = onBefore => {
-  const handle = event => {
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
-    }
-  };
+const useFadeIn = (duration = 3, delay = 0) => {
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
   }, []);
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => console.log("Pls dont leave");
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 10);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>lorem ipsum lalalala</p>
     </div>
   );
 };
